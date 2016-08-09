f70e7d41f5554e5d735a3c592d83c1ad5bf307c3
cr4dle <cr4dle@gmail.com>
2016-08-09 02:33:28 +0100

    Add $filter and draw our first line

--- Graphic/scripts/index.js
+++ Graphic/scripts/index.js
@@ -1,5 +1,5 @@
 ﻿angular.module('App', [])
-.controller('TodoCtrl', function ($scope, $http) {
+.controller('TodoCtrl', function ($scope, $http, $filter) {
 
     $scope.showMinimum = function () {
         let minimum = $scope.getMinimum($scope.data, 'amount');
@@ -30,6 +30,10 @@
             alert('The field \'amount\' is not part of the data object');
         }
     };
+
+    $scope.plot = function () {
+        $scope.todo($scope.graphicProperties, $scope.data, 'date', 'amount');
+    }
 
     $scope.getMinimum = function (data, basedField) {
         return $scope.basicHelpers(data, basedField, 'min');
@@ -65,6 +69,100 @@
         return result;
     };
 
+    //todo: split in small methods
+    //$scope.setScale = function (data, domain, range) {
+    //    var x = d3.scaleTime()
+    //        .range($scope.basicHelpers(data, domain, 'both'));
+
+    //    var y = d3.scaleLinear()
+    //        .range($scope.basicHelpers(data, range, 'both'));
+
+    //    var line = d3.line()
+    //        .x(function (d) { return x(d[domain]); })
+    //        .y(function (d) { return x(d[range]); });
+
+    //    return line;
+    //};
+    //todo: split in small methods
+    //$scope.svg = function (graphicProperties) {
+    //    var svg = d3.select("body").append("svg")
+    //        .attr("width", graphicProperties.width + graphicProperties.margin.left +graphicProperties.margin.right)
+    //        .attr("height", graphicProperties.height + graphicProperties.margin.top + graphicProperties.margin.bottom)
+    //        .append("g")
+    //        .attr("transform", "translate(" + graphicProperties.margin.left + "," + graphicProperties.margin.top + ")");
+
+    //    return svg;
+    //};
+
+    //todo: split in small methods
+    $scope.todo = function (graphicProperties, data, domain, range) {
+        ///
+        var x = d3.scaleTime()
+            .range([0, graphicProperties.width]);
+
+        var y = d3.scaleLinear()
+            .range([graphicProperties.height, 0]);
+
+        var line = d3.line()
+            .x(function (d) { return x(d.date); })
+            .y(function (d) { return y(d.value); });
+
+        ///
+        var svg = d3.select("body").append("svg")
+            .attr("width", graphicProperties.width + graphicProperties.margin.left + graphicProperties.margin.right)
+            .attr("height", graphicProperties.height + graphicProperties.margin.top + graphicProperties.margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + graphicProperties.margin.left + "," + graphicProperties.margin.top + ")");
+
+        ///
+        d3.json("../json/tSample.json", function (error, data) {
+            if (error) throw error;
+
+            let graphicData = $scope.resolveData(data, 'date', 'amount');
+
+            let value = graphicData[0];//.slice(0,12);
+                x.domain(d3.extent(value, function (d) { return d.date; }));
+                y.domain(d3.extent(value, function (d) { return d.value; }));
+
+                svg.append("g")
+                    .attr("class", "axis axis--x")
+                    .attr("transform", "translate(0," + $scope.graphicProperties.height + ")")
+                    .call(d3.axisBottom(x));
+
+                svg.append("g")
+                    .attr("class", "axis axis--y")
+                    .call(d3.axisLeft(y))
+                  .append("text")
+                    .attr("class", "axis-title")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 6)
+                    .attr("dy", ".71em")
+                    .style("text-anchor", "end");
+
+                svg.append("path")
+                    .datum(value)
+                    .attr("class", "line")
+                    .attr("d", line)
+                    .attr('stroke', 'green')
+                    .attr('stroke-width', 5)
+                    .attr('fill', 'none');
+
+                //svg.append("path")
+                //    .datum(graphicData[1])
+                //    .attr("class", "line")
+                //    .attr("d", line)
+                //    .attr('stroke', 'blue')
+                //    .attr('stroke-width', 5)
+                //    .attr('fill', 'none');
+
+        });
+    };
+    
+    $scope.fieldExists = function (dataRow, field) {
+        return dataRow[field] !== undefined;
+    }
+
+    // Methods to prepare the data for the graphic
     $scope.prepareData = function (data, groupBy) {
         return $filter('orderBy')(data.reduce(function (previousValue, currentValue, index, array) {
             var tempDate = new Date(currentValue[groupBy]);
@@ -84,6 +182,7 @@
         }, []), 'groupBy');
     };
 
+    // Get an array for each line we need to draw
     $scope.resolveData = function (data, groupBy, field) {
         let graphic1 = [], graphic2 = [];
 
@@ -103,18 +202,16 @@
 
         return [graphic1, graphic2];
     };
-    
-    $scope.fieldExists = function (dataRow, field) {
-        return dataRow[field] !== undefined;
-    }
 
-    $scope.getSample = function () {
-        $http.get('./json/tSample.json')
-         .then(function (res) {
-             $scope.data = res.data;
-        });
+    // Graphic properties
+    $scope.margin = { top: 20, right: 20, bottom: 30, left: 50 };
+
+    $scope.graphicProperties = {
+        margin: $scope.margin,
+        width: 960 - $scope.margin.left - $scope.margin.right,
+        height: 500 - 20 - 30
     };
 
-    // Triggers get the data
-    $scope.getSample();
+    // Triggers the draw
+    $scope.plot();
 });
